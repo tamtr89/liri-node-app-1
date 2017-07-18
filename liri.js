5b1a34e4802d32596f8cb06889e215a5178ff976
justincone <me@justincone.com>
2017-07-18 12:42:13 -0500

    Basic swich structure for commands

--- liri.js
+++ liri.js
@@ -1,2 +1,22 @@
+// Initialization
 var keys = require("./keys");
-console.log(keys);
+
+// User input
+var command = process.argv[2];
+
+switch(command) {
+  case "my-tweets":
+    console.log("You're trying to fetch tweets.");
+    break;
+  case "spotify-this-song":
+    console.log("You're trying to use Spotify.");
+    break;
+  case "movie-this":
+    console.log("You're trying to query OMDB.");
+    break;
+  case "do-what-it-says":
+    console.log("You're trying to do what it says.");
+    break;
+  default:
+    console.log("Sorry, I don't know how to do that yet.");
+}
