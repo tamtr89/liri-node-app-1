d18ecf5e342ebe2976ba164d6010838fa15b2f57
justincone <me@justincone.com>
2017-07-18 12:36:25 -0500

    Requiring keys

---
+++ keys.js
@@ -0,0 +1,7 @@
+console.log("Keys loaded!");
+exports.twitterKeys = {
+  "consumer_key": "5yOrjyylaLFWBvilngtRbBxmI",
+  "consumer_secret": "3qeAEvwjEwbm4evfQxUv1PkQs42D0yECgpju6Vw9rpt08DbxYS",
+  "access_token_key": "887360596462776320-B91k7fjjOjV68mtXikp9cO4VYagFJ6n",
+  "access_token_secret": "Q4C95JI1A1vnwtSJTItUg3oC8KT9rBfwINSSJoqYqpVV1",
+}
